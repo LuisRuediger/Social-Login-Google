5f166f058371ca19161e90c78d442975caed9c6a
Luis Ruediger <luisruediger27@gmail.com>
2022-01-12 13:50:19 -0300

    Add Google Login

--- script.js
+++ script.js
@@ -34,3 +34,36 @@ form.addEventListener("animationend", (event) => {
     document.querySelector("body").style.overflow = "none";
   }
 });
+
+function handleCredentialResponse(response) {
+  const data = jwt_decode(response.credential);
+  console.log(data);
+
+  fullName.textContent = data.name;
+  sub.textContent = data.sub;
+  given_name.textContent = data.given_name;
+  family_name.textContent = data.family_name;
+  email.textContent = data.email;
+  verified_email.textContent = data.verified_email;
+  picture.setAttribute("src", data.picture);
+}
+window.onload = function () {
+  google.accounts.id.initialize({
+    client_id:
+      "223772553314-978c5q3in5djtch7u0s1d4fdhukco7cn.apps.googleusercontent.com",
+    callback: handleCredentialResponse,
+  });
+  google.accounts.id.renderButton(
+    document.getElementById("buttonDiv"),
+    {
+      theme: "filled_black",
+      size: "large",
+      type: "standard",
+      shape: "pill",
+      text: "signin_with",
+      locale: "en-US",
+      logo_alignment: "left",
+    } // customization attributes
+  );
+  google.accounts.id.prompt(); // also display the One Tap dialog
+};
